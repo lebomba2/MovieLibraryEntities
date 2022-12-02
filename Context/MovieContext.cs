247d6ab95de87583841677a8409453117972592a
Chris Lebombard <lebomba2@gmail.com>
2022-12-02 15:29:33 -0600

    Did some minor edits and removed some dead code.

--- Context/MovieContext.cs
+++ Context/MovieContext.cs
@@ -6,12 +6,12 @@ namespace MovieLibraryEntities.Context
 {
     public class MovieContext : DbContext
     {
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<MovieGenre> MovieGenres { get; set; }
-        public DbSet<Occupation> Occupations { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserMovie> UserMovies { get; set; }
+        public DbSet<Genre>? Genres { get; set; }
+        public DbSet<Movie>? Movies { get; set; }
+        public DbSet<MovieGenre>? MovieGenres { get; set; }
+        public DbSet<Occupation>? Occupations { get; set; }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<UserMovie>? UserMovies { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -20,7 +20,7 @@ namespace MovieLibraryEntities.Context
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-            optionsBuilder.UseSqlServer(
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(
                 configuration.GetConnectionString("MovieContext")
             );
         }
